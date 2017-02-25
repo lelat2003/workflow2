4f1c1bdc0990c224c7635a7531830320ad6ecf5c
T.R <lelat2003@gmail.com>
2017-02-25 12:00:55 -0500

    browser-sync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
+var SOURCEPATHS = {
+    saaSource: 'src/scss/*.scss'
+}
+
+var APPPATH = {
+    root: 'app',
+    css: 'app/css',
+    js: 'app/js' 
+}
 
 
 gulp.task('sass', function() {
-    return gulp.src('src/scss/app.scss')
+    return gulp.src(SOURCEPATHS.saaSource)
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest(APPPATH.css));
 });
 
+gulp.task('serve', ['sass'], function() {
+    browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+    server: {
+        baseDir: APPPATH.root
+    }
+    });
+});
 
-gulp.task('default', ['sass']);
+gulp.task('watch', ['serve', 'sass'], function() {
+    gulp.watch([SOURCEPATHS.saaSource], ['sass']);
+});
+
+gulp.task('default', ['watch']);
